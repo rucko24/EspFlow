83b93f7bd5a57da7162ac835117cf2e70e1ed285
start.vaadin.com <no-reply@vaadin.com>
2022-07-23 20:58:28 +0000

    Generated project

---
+++ src/main/java/com/nodemcutools/application/Application.java
@@ -0,0 +1,30 @@
+package com.nodemcutools.application;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@NpmPackage(value = "@fontsource/cairo", version = "4.5.0")
+@Theme(value = "nodemcuuitool")
+@PWA(name = "NodeMCU UI Tool", shortName = "NodeMCU UI Tool", offlineResources = {})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+@NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/nodemcutools/application/data/Role.java
@@ -0,0 +1,5 @@
+package com.nodemcutools.application.data;
+
+public enum Role {
+    USER, ADMIN;
+}
