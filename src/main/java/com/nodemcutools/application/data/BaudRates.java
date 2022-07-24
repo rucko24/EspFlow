dbfb815cad920418c4cbf4d30c6aa23a3ad62b9b
rucko24 <rucko24@gmail.com>
2022-07-24 03:50:36 +0200

    Enums FlashMode and BaudRates

---
+++ src/main/java/com/nodemcutools/application/data/BaudRates.java
@@ -0,0 +1,30 @@
+package com.nodemcutools.application.data;
+
+import jssc.SerialPort;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author rubn
+ */
+@Getter
+@RequiredArgsConstructor
+public enum BaudRates {
+
+    BAUD_RATE_9600(SerialPort.BAUDRATE_9600),
+    BAUD_RATE_57600(SerialPort.BAUDRATE_57600),
+    BAUD_RATE_74880(74880),
+    BAUD_RATE_115200(SerialPort.BAUDRATE_115200),
+    BAUD_RATE_230400(230400),
+    BAUD_RATE_460800(460800),
+    BAUD_RATE_921600(921600);
+
+    private final int baudRate;
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(baudRate);
+        return sb.toString();
+    }
+}
