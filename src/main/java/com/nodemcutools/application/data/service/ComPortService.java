7697eab7eb376cfb8a1c8b7ad1dc9fafa7149803
rucko24 <rucko24@gmail.com>
2022-07-25 23:48:03 +0200

    :construction: Updated comport service

--- src/main/java/com/nodemcutools/application/data/service/ComPortService.java
+++ src/main/java/com/nodemcutools/application/data/service/ComPortService.java
@@ -1,10 +1,5 @@
 package com.nodemcutools.application.data.service;
 
-
-import com.nodemcutools.application.data.util.Notification;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.notification.Notification.Position;
-import com.vaadin.flow.component.notification.NotificationVariant;
 import jssc.SerialPortList;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -30,41 +25,14 @@ public class ComPortService {
             Arrays.stream(SerialPortList.getPortNames())
                     .forEach(portList::add);
             if (!portList.isEmpty()) {
-                Notification.builder()
-                        .withText("Port found!")
-                        .withPosition(Position.MIDDLE)
-                        .withDuration(3000)
-                        .withIcon(VaadinIcon.WARNING)
-                        .withThemeVariant(NotificationVariant.LUMO_PRIMARY)
-                        .make();
+                log.info("Port found! {}", portList);
             } else {
-                Notification.builder()
-                        .withText("Port not found!")
-                        .withPosition(Position.MIDDLE)
-                        .withDuration(3000)
-                        .withIcon(VaadinIcon.WARNING)
-                        .withThemeVariant(NotificationVariant.LUMO_ERROR)
-                        .make();
+                log.info("Port not found! {}", portList);
             }
         } catch (UnsatisfiedLinkError e) {
             log.error("Error UnsatisfiedLinkError: {}", e);
-            Notification.builder()
-                    .withText("UnsatisfiedLinkError")
-                    .withPosition(Position.MIDDLE)
-                    .withDuration(3000)
-                    .withIcon(VaadinIcon.WARNING)
-                    .withThemeVariant(NotificationVariant.LUMO_ERROR)
-                    .make();
-
         } catch (NoClassDefFoundError ee) {
             log.error("Error NoClassDefFoundError: {}", ee);
-            Notification.builder()
-                    .withText("NoClassDefFoundError")
-                    .withPosition(Position.MIDDLE)
-                    .withDuration(3000)
-                    .withIcon(VaadinIcon.WARNING)
-                    .withThemeVariant(NotificationVariant.LUMO_ERROR)
-                    .make();
         }
         return portList;
     }
