7e860d1b983d04aa90164197a3f09072633ca846
rucko24 <rucko24@gmail.com>
2022-07-25 23:44:18 +0200

    process dmesg | grep tty

--- src/main/java/com/nodemcutools/application/data/service/CommandService.java
+++ src/main/java/com/nodemcutools/application/data/service/CommandService.java
@@ -14,6 +14,10 @@ import reactor.core.scheduler.Schedulers;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.nodemcutools.application.data.util.UiToolConstants.DMESG_TTY;
 
 /**
  * @author rubn
@@ -30,7 +34,7 @@ public class CommandService {
         return this.processInputStream("esptool.py", "version")
                 .map((String line) -> {
                     final String esp = line.split(" ")[0];
-                    final String numberVersion = line.split("\n")[1];
+                    final String numberVersion = line.split(System.lineSeparator())[1];
                     return esp.concat(" v")
                             .concat(numberVersion);
                 });
@@ -38,17 +42,14 @@ public class CommandService {
 
     @SneakyThrows
     public Process execute(String... commands) {
-        final Process process = new ProcessBuilder()
+        return new ProcessBuilder()
                 .command(commands)
                 .redirectErrorStream(Boolean.TRUE)
                 .start();
-        //set this processs to pid field
-        this.listProcess.add(process.pid());
-        return process;
     }
 
     public void killProcess(final Long pid) {
-        if(listProcess.contains(pid)) {
+        if (listProcess.contains(pid)) {
             log.info("killing pid: {}", listProcess);
             this.execute("kill", pid.toString());
         }
@@ -56,7 +57,6 @@ public class CommandService {
 
     /**
      * @param commands
-     *
      * @return Flux<String>
      */
     public Flux<String> processInputStream(final String... commands) {
@@ -69,6 +69,21 @@ public class CommandService {
                     DataBufferUtils.release(dataBuffer);
                     return new String(bytes, StandardCharsets.UTF_8);
                 }).subscribeOn(Schedulers.boundedElastic());
+    }
+
+    public Flux<String> executeDmesgForTtyPort() {
+        return processInputStream(DMESG_TTY)
+                .map((String line) -> line.split(System.lineSeparator()))
+                .map((String[] tmp) -> Stream.of(tmp)
+                        .map((String line) -> {
+                            if (line.contains("tty")) {
+                                return "\n".concat(line)
+                                        .replace("\\s+", "");
+                            }
+                            return "";
+                        })
+                        .collect(Collectors.joining())
+                );
     }
 
 }
