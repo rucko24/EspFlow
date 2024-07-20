1f9f09ea6dfa7876a77b8c94484e1df539920462
rucko <rucko24@gmail.com>
2024-07-21 01:13:39 +0200

    :construction: Test #62 read_flash on Windows

--- src/test/java/com/esp/espflow/data/service/EsptoolServiceTest.java
+++ src/test/java/com/esp/espflow/data/service/EsptoolServiceTest.java
@@ -1,6 +1,7 @@
 package com.esp.espflow.data.service;
 
 import com.esp.espflow.data.entity.EspDeviceInfo;
+import com.esp.espflow.data.enums.BaudRates;
 import com.esp.espflow.data.util.GetOsName;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
@@ -14,7 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
-import static com.esp.espflow.data.util.EspFlowConstants.ESPTOOL_PY_VERSION;
+import static com.esp.espflow.data.util.EspFlowConstants.*;
 import static org.mockito.Mockito.when;
 
 /**
@@ -74,6 +75,37 @@ class EsptoolServiceTest {
 
         StepVerifier.create(esptoolService.showEsptoolVersion())
                 .expectNext("esptool.py v4.7.0")
+                .verifyComplete();
+
+    }
+
+    @Test
+    @DisplayName("read raw each String from this inputStream")
+    void  readRawFlashIdFromPort() {
+
+        String[] commands = ArrayUtils.addAll(
+                GetOsName.shellOsName(),
+                ESPTOOL_PY,
+                PORT, "COM3",
+                BAUD_RATE, String.valueOf(BaudRates.BAUD_RATE_115200.getBaudRate()),
+                FLASH_ID);
+
+        Flux<String> actual = Flux.just(
+                "Detecting chip type... ESP32-S3",
+                "Chip is ESP32-S3 (QFN56) (revision v0.0)",
+                "Detected flash size: 8MB");
+
+        when(commandService.processIntputStreamLineByLine(commands))
+                .thenReturn(actual);
+
+        String expetedFirtsLine = "Detecting chip type... ESP32-S3";
+        String expetedSecondLine = "Chip is ESP32-S3 (QFN56) (revision v0.0)";
+        String expetedThirdLine = "Detected flash size: 8MB";
+
+        StepVerifier.create(this.esptoolService.readRawFlashIdFromPort(commands))
+                .expectNext(expetedFirtsLine)
+                .expectNext(expetedSecondLine)
+                .expectNext(expetedThirdLine)
                 .verifyComplete();
 
     }
