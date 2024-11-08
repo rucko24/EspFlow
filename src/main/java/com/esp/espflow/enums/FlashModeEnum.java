85114927ddb598626b73aaa70c3bf6f77f25a170
rucko <rucko24@gmail.com>
2024-11-09 00:37:27 +0100

    :construction: #78 simplification with switch

--- src/main/java/com/esp/espflow/enums/FlashModeEnum.java
+++ src/main/java/com/esp/espflow/enums/FlashModeEnum.java
@@ -43,29 +43,18 @@ public enum FlashModeEnum {
     /**
      *
      * @param flashModeEnum
-     * @return Span
+     *
+     * @return A {@link Span}
      */
     private static Span createSpan(final FlashModeEnum flashModeEnum) {
         final Span span = new Span(String.valueOf(flashModeEnum.getFlashMode()));
-
-        if(FlashModeEnum.QUAD_IO == flashModeEnum) {
-
-            Tooltip.forComponent(span).setText("SPI host uses the \"Quad I/O Fast Read\" command (EBh). Four SPI pins are used to write the flash address part of the command, and to read flash data out. Therefore these phases need a quarter the clock cycles compared to standard SPI.");
-
-        } else if(FlashModeEnum.Q_OUT == flashModeEnum) {
-
-            Tooltip.forComponent(span).setText("SPI host uses the \"Quad Output Fast Read\" command (6Bh) Four SPI pins are used to read the flash data out. Slightly slower than QIO, because the address is written via the single MOSI data pin.");
-
-        } else if(FlashModeEnum.DUAL_IO == flashModeEnum) {
-
-            Tooltip.forComponent(span).setText("Default, SPI host uses the \"Dual I/O Fast Read\" command (BBh).  Two SPI pins are used to write the flash address part of the command, and to read flash data out. Therefore these phases need half the clock cycles compared to standard SPI.");
-
-        } else {// DUAL_OUTPUT
-
-            Tooltip.forComponent(span).setText("SPI host uses the \"Dual Output Fast Read\" command (3Bh).Two SPI pins are used to read flash data out. Slightly slower than DIO, because the address is written via the single MOSI data pin.");
-
-        }
-
+        switch (flashModeEnum) {
+            case QUAD_IO -> Tooltip.forComponent(span).setText("SPI host uses the \"Quad I/O Fast Read\" command (EBh). Four SPI pins are used to write the flash address part of the command, and to read flash data out. Therefore these phases need a quarter the clock cycles compared to standard SPI.");
+            case Q_OUT -> Tooltip.forComponent(span).setText("SPI host uses the \"Quad Output Fast Read\" command (6Bh) Four SPI pins are used to read the flash data out. Slightly slower than QIO, because the address is written via the single MOSI data pin.");
+            case DUAL_IO -> Tooltip.forComponent(span).setText("Default, SPI host uses the \"Dual I/O Fast Read\" command (BBh).  Two SPI pins are used to write the flash address part of the command, and to read flash data out. Therefore these phases need half the clock cycles compared to standard SPI.");
+            // DUAL_OUTPUT
+            default -> Tooltip.forComponent(span).setText("SPI host uses the \"Dual Output Fast Read\" command (3Bh).Two SPI pins are used to read flash data out. Slightly slower than DIO, because the address is written via the single MOSI data pin.");
+        };
         return span;
     }
 }
