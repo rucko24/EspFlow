788e105c3d895c88e7ed2fa5823155080e9485bc
rubn <rucko24@gmail.com>
2024-12-19 10:45:22 +0100

    resolve #96

--- src/main/java/com/esp/espflow/entity/event/EsptoolFRWMessageListItemEvent.java
+++ src/main/java/com/esp/espflow/entity/event/EsptoolFRWMessageListItemEvent.java
@@ -1,16 +1,12 @@
 package com.esp.espflow.entity.event;
 
-import com.esp.espflow.util.svgfactory.SvgFactory;
 import com.vaadin.flow.component.messages.MessageListItem;
-import com.vaadin.flow.server.StreamResource;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-
-import static com.esp.espflow.util.EspFlowConstants.FRONTEND_IMAGES_SVG_ICONS;
 
 /**
  *
@@ -47,13 +43,7 @@ public class EsptoolFRWMessageListItemEvent extends MessageListItem {
         super.setText(operation);
         super.setTime(LocalDateTime.now().toInstant(ZoneOffset.UTC));
         super.setUserName(port);
-        super.setUserImageResource(setCustomImage());
-    }
-
-    private static StreamResource setCustomImage() {
-        return new StreamResource("usb-port-black.svg",
-                () -> SvgFactory.class.getResourceAsStream(FRONTEND_IMAGES_SVG_ICONS + "usb-port-black.svg"));
-
+        super.setUserImage("frontend/images/svg-icons/" + "usb-port-black.svg");
     }
 
 }
--- src/main/java/com/esp/espflow/entity/event/EsptoolVersionMessageListItemEvent.java
+++ src/main/java/com/esp/espflow/entity/event/EsptoolVersionMessageListItemEvent.java
@@ -1,8 +1,6 @@
 package com.esp.espflow.entity.event;
 
-import com.esp.espflow.util.svgfactory.SvgFactory;
 import com.vaadin.flow.component.messages.MessageListItem;
-import com.vaadin.flow.server.StreamResource;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -11,7 +9,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
 import static com.esp.espflow.util.EspFlowConstants.EXECUTABLE_ICON;
-import static com.esp.espflow.util.EspFlowConstants.FRONTEND_IMAGES_SVG_ICONS;
 
 /**
  * @author rubn
@@ -26,7 +23,7 @@ public class EsptoolVersionMessageListItemEvent extends MessageListItem {
         BUNDLED("Loaded bundled esptool"),
         CUSTOM("Loaded custom esptool");
 
-        private final String getExecutableType;
+        private final String executableType;
 
     }
 
@@ -42,13 +39,8 @@ public class EsptoolVersionMessageListItemEvent extends MessageListItem {
         super.setText(esptoolVersion.concat(" ")
                 .concat(absolutePathEsptool));
         super.setTime(LocalDateTime.now().toInstant(ZoneOffset.UTC));
-        super.setUserName(loadedType.getGetExecutableType());
-        super.setUserImageResource(setCustomImage());
-    }
-
-    private static StreamResource setCustomImage() {
-        return new StreamResource(EXECUTABLE_ICON,
-                () -> SvgFactory.class.getResourceAsStream(FRONTEND_IMAGES_SVG_ICONS + EXECUTABLE_ICON));
+        super.setUserName(loadedType.getExecutableType());
+        super.setUserImage("frontend/images/svg-icons/" + EXECUTABLE_ICON);
     }
 
 }
