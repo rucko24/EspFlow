46a92999b469c7540787702367e362e14184b10d
rubn <rucko24@gmail.com>
2025-04-20 17:31:33 +0200

    resolve v1 splash

--- src/main/java/com/esp/espflow/Application.java
+++ src/main/java/com/esp/espflow/Application.java
@@ -1,18 +1,15 @@
 package com.esp.espflow;
 
-import com.esp.espflow.service.respository.UserRepository;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.LoadingIndicatorConfiguration;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinServiceInitListener;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
-import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import javax.sql.DataSource;
 
 /**
  * The entry point of the Spring Boot application.
@@ -25,25 +22,17 @@ import javax.sql.DataSource;
 @Theme(value = "espflow")
 @EnableAsync(proxyTargetClass = true)
 @PWA(name = "EspFlow", shortName = "EspFlow", offlineResources = {})
-public class Application implements AppShellConfigurator {
+public class Application implements AppShellConfigurator, VaadinServiceInitListener {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    SqlDataSourceScriptDatabaseInitializer dataSourceScriptDatabaseInitializer(DataSource dataSource,
-                                                                               SqlInitializationProperties properties, UserRepository repository) {
-        // This bean ensures the database is only initialized when empty
-        return new SqlDataSourceScriptDatabaseInitializer(dataSource, properties) {
-            @Override
-            public boolean initializeDatabase() {
-                if (repository.count() == 0L) {
-                    return super.initializeDatabase();
-                }
-                return false;
-            }
-        };
+    @Override
+    public void serviceInit(ServiceInitEvent event) {
+        event.getSource().addUIInitListener(ui -> {
+            LoadingIndicatorConfiguration conf = ui.getUI().getLoadingIndicatorConfiguration();
+            conf.setApplyDefaultTheme(false);
+        });
     }
-
 }
