a13e3c0544c993540e14bb07694ffd25b0f4e1d2
rubn <rucko24@gmail.com>
2025-12-19 13:05:24 +0100

    :memo: css

---
+++ src/main/java/com/esp/espflow/views/hexdump/InputGroup.java
@@ -0,0 +1,30 @@
+package com.esp.espflow.views.hexdump;
+
+import com.esp.espflow.views.Layout;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+public class InputGroup extends Layout {
+
+    public static final String INPUT_GROUP = "input-group";
+
+    public InputGroup(Component... components) {
+        super(components);
+        addClassName(INPUT_GROUP);
+        setFlexDirection(FlexDirection.ROW);
+
+        for (Component component : components) {
+            component.addClassNames(LumoUtility.MinWidth.NONE);
+        }
+    }
+
+    @Override
+    public void setFlexDirection(FlexDirection flexDirection) {
+        super.setFlexDirection(flexDirection);
+        if (flexDirection.equals(FlexDirection.COLUMN)) {
+            setAlignItems(AlignItems.STRETCH);
+        } else {
+            setAlignItems(AlignItems.BASELINE);
+        }
+    }
+}
